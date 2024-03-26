24bf7b477b70014f9f4cbbd2c8add5aba361812f
Arthur Machado <arthur.machado@maxmilhas.com.br>
2024-03-26 09:32:16 -0300

    feat: add routers

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,17 @@
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from './styles/themes/default'
+import { Router } from './Router'
+import { GlobalStyle } from './styles/global'
+import { BrowserRouter } from 'react-router-dom'
+
 export function App() {
-  return <h1>Hello World</h1>
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+
+      <GlobalStyle />
+    </ThemeProvider>
+  )
 }
