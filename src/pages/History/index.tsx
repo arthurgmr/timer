aa73378f1fdce7ba7d21d465462b707f485f9ab0
Arthur Machado <arthur.machado@maxmilhas.com.br>
2024-04-02 10:04:01 -0300

    feat: history style

---
+++ src/pages/History/index.tsx
@@ -0,0 +1,31 @@
+import { HistoryContainer, HistoryList } from './styles'
+
+export function History() {
+  return (
+    <HistoryContainer>
+      <h1>My History</h1>
+
+      <HistoryList>
+        <table>
+          <thead>
+            <tr>
+              <th>Task</th>
+              <th>Duration</th>
+              <th>Start</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            <tr>
+              <td>Do homework</td>
+              <td>28 minutes</td>
+              <td>Two weeks ago</td>
+              <td>Concluded</td>
+            </tr>
+          </tbody>
+        </table>
+      </HistoryList>
+    </HistoryContainer>
+  )
+}
